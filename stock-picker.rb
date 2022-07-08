537b289da323f3d8f8ead9d41ee0593bbca3b1ba
BillC424 <wcurran44@gmail.com>
2022-07-08 19:25:53 -0400

    Updated nested enumerable method to include the index of each element and to add the difference of each stock sale as the key and the indexes of the stock purchase and sale days as the value in a Hash. Added max_by method so that the value of the highest value key was returned.

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,10 +1,11 @@
 def stock_picker(prices)
-    result = Hash.new(0)
+    best_days_to_buy_and_sell = Hash.new(0)
     prices.each_with_index do |buy_price, index|
-      prices[index..].each do |sell_price|
-         sell_price - buy_price
+      prices[index..].each_with_index do |sell_price, index2|
+          best_days_to_buy_and_sell[sell_price - buy_price] = [index, index2 + 1]
       end
-    end   
+    end  
+   best_days_to_buy_and_sell.max_by {|key, value| key}[1]
   end
   
   p stock_picker([17,3,6,9,15,8,6,1,10])
