ad183886c73fe5f24bed12f778e6c582b20aca94
BillC424 <wcurran44@gmail.com>
2022-07-08 19:51:02 -0400

    Updated nested enumerable so that the index of the sell_price is being accessed instead of adding a number to the index number being returned from each_with_index

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,8 +1,8 @@
 def stock_picker(prices)
     best_days_to_buy_and_sell = Hash.new(0)
     prices.each_with_index do |buy_price, index|
-      prices[index..].each_with_index do |sell_price, index2|
-          best_days_to_buy_and_sell[sell_price - buy_price] = [index, index2 + 1]
+      prices[index..].each do |sell_price|
+          best_days_to_buy_and_sell[sell_price - buy_price] = [index, prices.index(sell_price)]
       end
     end  
    best_days_to_buy_and_sell.max_by {|key, value| key}[1]
